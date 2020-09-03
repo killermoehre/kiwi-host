c2241491584100eedd640c858f764639665539ae
Silvio Knizek <silvio.knizek@sap.com>
2020-09-03 11:43:05 +0200

    use actual partitions instead of {,PART}LABELS

--- create-image.sh
+++ create-image.sh
@@ -16,17 +16,11 @@ fallocate -l 5GiB kiwi-host.img
 sgdisk -n 0:0:+512M -t 0:ef00 -c 0:KIWI_BOOT -n 0:0:0 -t 0:8304 -c KIWI_ROOT kiwi-host.img
 _loop_dev="$(sudo losetup --show -f -P kiwi-host.img)"
 partprobe -s "$_loop_dev"
-until test -e /dev/disk/by-partlabel/KIWI_BOOT; do
-    sleep 0.2
-done
-mkfs.vfat -F32 -n KIWI_BOOT /dev/disk/by-partlabel/KIWI_BOOT
-until test -e /dev/disk/by-partlabel/KIWI_ROOT; do
-    sleep 0.2
-done
-mkfs.xfs -L KIWI_ROOT /dev/disk/by-partlabel/KIWI_ROOT
-mount LABEL=KIWI_ROOT /mnt
+mkfs.vfat -F32 -n KIWI_BOOT "${_loop_dev}p1"
+mkfs.xfs -L KIWI_ROOT "${_loop_dev}p2"
+mount "${_loop_dev}p1" /mnt
 mkdir /mnt/boot
-mount LABEL=KIWI_BOOT /mnt/boot
+mount "${_loop_dev}p2" /mnt/boot
 cdebootstrap -f minimal focal /mnt
 sd-apt-get -y update
 sd-apt-get -y install dracut dracut-config-generic linux-image-generic
