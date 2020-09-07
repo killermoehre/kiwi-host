a274a40ddfecdaf8fb487771c55d473bc13d4dd1
Silvio Knizek <silvio.knizek@sap.com>
2020-09-07 16:17:08 +0200

    Adding debug statements and fixing a few missed /mnt

--- create-image.sh
+++ create-image.sh
@@ -24,21 +24,31 @@ declare -x -r DEBCONF_NONINTERACTIVE_SEEN='true'
 declare _mnt_tmp_dir
 _mnt_tmp_dir="$(mktemp -d)"
 
+echo "* Allocating Image of 5GiB"
 fallocate -l 5GiB kiwi-host.img
+echo "* Partitioning Image"
 sgdisk -n 0:0:+256M -t 0:ef00 -c 0:KIWI_BOOT -n 0:0:0 -t 0:8304 -c KIWI_ROOT kiwi-host.img
+echo "* Loop Mounting Image"
 _loop_dev="$(sudo losetup --show -f -P kiwi-host.img)"
 partprobe -s "$_loop_dev"
-mkfs.vfat -F32 -n KIWI_BOOT "${_loop_dev}p1"
-mkfs.xfs -L KIWI_ROOT "${_loop_dev}p2"
-mount "${_loop_dev}p1" "$_mnt_tmp_dir"
+echo "* Creating FAT32 on ${_loop_dev}p1"
+mkfs.vfat -F32 -n KIWI_BOOT "${_loop_dev}p1" || exit 1
+echo "* Creating XFS on ${_loop_dev}p2"
+mkfs.xfs -L KIWI_ROOT "${_loop_dev}p2" || exit 1
+echo "* Mounting ${_loop_dev}p1 to $_mnt_tmp_dir"
+mount "${_loop_dev}p1" "$_mnt_tmp_dir" || exit 1
+echo "* Mounting ${_loop_dev}p2 to $_mnt_tmp_dir/boot"
 mkdir "$_mnt_tmp_dir/boot"
-mount "${_loop_dev}p2" "$_mnt_tmp_dir/boot"
+mount "${_loop_dev}p2" "$_mnt_tmp_dir/boot" || exit 1
+echo "* Starting debootstrap into $_mnt_tmp_dir"
 debootstrap --variant=minbase \
     --merged-usr \
     --components=main,universe \
-    focal /mnt http://azure.archive.ubuntu.com/ubuntu
+    focal "$_mnt_tmp_dir" http://azure.archive.ubuntu.com/ubuntu
+echo "* Update and Install Packages into Image"
 sd-apt-get -y update
 sd-apt-get -y install "${_system_pkgs[@]}"
+echo "* Installing boot loader"
 sd-bootctl install
-umount /mnt/boot /mnt
+umount "$_mnt_tmp_dir/boot" "$_mnt_tmp_dir"
 losetup -D
