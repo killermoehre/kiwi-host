6620d0e5db672858b3cace1548bb16a8848c3157
Silvio Knizek <silvio.knizek@sap.com>
2020-09-07 11:34:14 +0200

    use a temp dir as mountpoint as /mnt is already in use

--- create-image.sh
+++ create-image.sh
@@ -15,15 +15,18 @@ function sd-bootctl () {
 # komma seperated list of additional packages
 declare -a _system_pkgs=('binutils' 'cloud-init' 'dbus' 'dracut' 'libpam-systemd' 'linux-generic' 'openssh-server' 'open-vm-tools' 'systemd')
 
+declare _mnt_tmp_dir
+_mnt_tmp_dir="$(mktemp -d)"
+
 fallocate -l 5GiB kiwi-host.img
 sgdisk -n 0:0:+256M -t 0:ef00 -c 0:KIWI_BOOT -n 0:0:0 -t 0:8304 -c KIWI_ROOT kiwi-host.img
 _loop_dev="$(sudo losetup --show -f -P kiwi-host.img)"
 partprobe -s "$_loop_dev"
 mkfs.vfat -F32 -n KIWI_BOOT "${_loop_dev}p1"
 mkfs.xfs -L KIWI_ROOT "${_loop_dev}p2"
-mount "${_loop_dev}p1" /mnt
-mkdir /mnt/boot
-mount "${_loop_dev}p2" /mnt/boot
+mount "${_loop_dev}p1" "$_mnt_tmp_dir"
+mkdir "$_mnt_tmp_dir/boot"
+mount "${_loop_dev}p2" "$_mnt_tmp_dir/boot"
 debootstrap --variant=minbase \
     --merged-usr \
     --components=main,universe \
